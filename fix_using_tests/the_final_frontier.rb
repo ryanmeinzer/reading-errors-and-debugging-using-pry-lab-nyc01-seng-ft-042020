90c14f1455b97dc0683051c49d8cf58780547d50
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-02 18:57:14 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -6,8 +6,8 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-def state_log(date)
-  "Captain's Log, star date #{date}."
+def state_log(star_date)
+  "Captain's Log, star date #{star_date}."
 end
 
 def engage
