aa9d0d385474eb949c3d826edf4ee44b96cecb1d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-02 18:24:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,11 +2,11 @@
 
 require 'pry'
 
-def start_game(player1, player2) 
+def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
-binding.pry
+# binding.pry
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
